78b39d5f05b5526423695ca50e94c64849327847
Yodgorovna <lobargoziyeva9@gmail.com>
2024-02-05 15:52:42 +0500

    medias are compleates

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/******************BACKTOP*********************/
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = "20px";
+  } else {
+    backtop.style.bottom = "-50px";
+  }
+}
+
+// RESPONSIVE NAVBAR //////
+function toggleNavbar() {
+  document.getElementById("navbar-responsive").classList.toggle("open");
+}
+
+document.getElementById("menu-btn").addEventListener("click", toggleNavbar);
